f086eae74a3da8432f714933f8ef4a1b2591c9d6
BiraHH <fdr190@gmail.com>
2018-03-19 19:59:30 -0300

    # Second Commit

---
+++ ReallyLongName/Assets/Scripts/Base/LPBaseObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (BoxCollider2D))]
+public class LPBaseObject : MonoBehaviour 
+{
+
+	public BoxCollider2D collider;
+
+	void Start () 
+	{
+		
+	}
+
+
+	void Update () 
+	{
+		
+	}
+}
