28fce36be2f372f0543ad2fe20d3a00dc6e42e33
BiraHH <fdr190@gmail.com>
2018-03-19 14:46:25 -0300

    # First Commit # Project Refactoring # Gitignore Setup

---
+++ ReallyLongName/Assets/Scripts/LPAudioController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LPAudioController : MonoBehaviour {
+
+
+	public AudioSource[] EnemyShoot;
+	public AudioSource[] EnemySmasher; 
+
+
+	void Start () 
+	{
+		
+	}
+
+
+	void Update () 
+	{
+		
+	}
+}
