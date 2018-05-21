c5c272b24bae75597ecbf62fb96d1ea604e8dcab
BiraHH <fdr190@gmail.com>
2018-05-21 20:04:52 -0300

    # Scene Transition - Assets

---
+++ ReallyLongName/Assets/Scripts/Aux/LoadingTextAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingTextAnimation : MonoBehaviour 
+{
+	string[] text;
+	string baseText;
+
+	void Start () 
+	{
+		text = new string[3];
+
+		text[0] = ".";
+		text[1] = "..";
+		text[2] = "...";
+
+		baseText = GetComponent<Text>().text;
+	}
+	
+
+	void Update () 
+	{
+	}
+}
